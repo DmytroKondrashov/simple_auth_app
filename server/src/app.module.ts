5abacb22fc3d34c38dc766603635e9f0c36e0ce2
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-23 12:00:46 +0300

    create user entity

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/user';
 
 @Module({
   imports: [
@@ -14,7 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: 'q1w2e3r4',
       database: 'simple_auth_app',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
   ],

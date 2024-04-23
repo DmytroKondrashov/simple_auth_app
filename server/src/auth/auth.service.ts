dfc34971a4e16f7fb57ef608c934290b35cce3d7
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-23 12:41:53 +0300

    update validateUser method

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -16,5 +16,7 @@ export class AuthService {
     console.log('=========================');
     const user = await this.userRepository.findOneBy({ email: details.email });
     if (user) return user;
+    const newUser = this.userRepository.create(details);
+    return this.userRepository.save(newUser);
   }
 }

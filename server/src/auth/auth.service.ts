4c653d215c7bac163fd79f014d25df43f28215b0
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-23 12:19:12 +0300

    add repository

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/user';
 import { UserDetails } from 'src/utils/types';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
   validateUser(details: UserDetails) {
     console.log('=========================');
     console.log(details);

27e675c23cca53f746bd76fbbbe3a6b2336b2bfc
DmytroKondrashov <kondrashovd101@gmail.com>
2024-04-23 10:27:37 +0300

    apply guard

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { GoogleAuthGuard } from './utils/guards';
 
 @Controller('auth')
 export class AuthController {
   @Get('google/login')
+  @UseGuards(GoogleAuthGuard)
   handleLogin() {
     return { msg: 'Google Authentication!' };
   }
 
   @Get('/google/redirect')
+  @UseGuards(GoogleAuthGuard)
   handleRedirect() {
     return { msg: 'OK!' };
   }

afdc9b953df6585986d4e01f4892fac196886636
DmytroKondrashov <kondrashovd101@gmail.com>
2024-05-05 10:33:13 +0300

    fix modules resolve issue

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { RouterProvider } from 'react-router-dom';
-import routes from './router/index';
+import { routes } from './router/index';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

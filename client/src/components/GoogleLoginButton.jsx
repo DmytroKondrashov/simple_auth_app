3e03cab7ac26cdce41792025df2fbe751997a703
DmytroKondrashov <kondrashovd101@gmail.com>
2024-05-21 14:02:31 +0300

    update url

--- client/src/components/GoogleLoginButton.jsx
+++ client/src/components/GoogleLoginButton.jsx
@@ -3,7 +3,7 @@ import { useGoogleLogin } from '@react-oauth/google';
 const GoogleLoginButton = () => {
   const login = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
-      const res = await fetch('http://localhost:3000/auth/google/callback', {
+      const res = await fetch('http://localhost:3001/auth/google/callback', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',

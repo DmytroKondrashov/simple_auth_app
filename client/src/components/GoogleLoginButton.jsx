9f8e30d79813314fc2311bb99f19fe97ddd12f28
DmytroKondrashov <kondrashovd101@gmail.com>
2024-05-21 12:11:09 +0300

    add GoogleLoginButton component

---
+++ client/src/components/GoogleLoginButton.jsx
@@ -0,0 +1,24 @@
+import { useGoogleLogin } from '@react-oauth/google';
+
+const GoogleLoginButton = () => {
+  const login = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      const res = await fetch('http://localhost:3000/auth/google/callback', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(tokenResponse),
+      })
+      const userData = res.json();
+      console.log(userData);
+    },
+    onError: (error) => {
+      console.error(error);
+    }
+  })
+
+  return <button onClick={() => login()}>Sign in with Google</button>
+}
+
+export default GoogleLoginButton;
